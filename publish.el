dd907952d4dece58d9eeda24b71c3dde3b12f39a
dojt <dotheis.work@gmail.com>
2026-03-20 12:06:18 +0200

    Attempt #0

---
+++ publish.el
@@ -0,0 +1,41 @@
+;; publish.el — run with: emacs --script publish.el
+
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(("my-site-content"
+         :base-directory "./org"          ; where your .org files live
+         :base-extension "org"
+         :publishing-directory "./docs"   ; output goes here
+         :recursive t                     ; include subdirectories
+         :publishing-function org-html-publish-to-html
+
+         ;; HTML options
+         :with-toc nil
+         :with-author t
+         :with-creator nil
+         :html-validation-link nil
+         :html-head-include-default-style nil   ; no Org default CSS
+         :html-head-include-scripts nil
+
+         ;; Optional: link to your own CSS
+         ;; :html-head "<link rel='stylesheet' href='/style.css'>"
+
+         ;; Optional: preamble/postamble
+         :html-preamble nil
+         :html-postamble nil)
+
+        ;; If you have static files (CSS, images), publish those too
+        ("my-site-static"
+         :base-directory "./org/static"
+         :base-extension "css\\|js\\|png\\|jpg\\|ico\\|svg"
+         :publishing-directory "./docs/static"
+         :recursive t
+         :publishing-function org-publish-attachment)
+
+        ;; Umbrella project that runs both
+        ("my-site"
+         :components ("my-site-content" "my-site-static"))))
+
+;; Force republish everything (skip cache)
+(org-publish "my-site" t)
